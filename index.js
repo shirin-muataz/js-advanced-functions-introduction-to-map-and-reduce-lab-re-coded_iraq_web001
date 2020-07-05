e2837ab5c22bd4e9aab21e1d4072bc63846070f4
Shirin Muataz <shirinmuataz@gmail.com>
2020-07-05 00:19:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Your code here
+function  mapToNegativize(sourceArray){
+ let newArr= [];
+ for(let i = 0; i < sourceArray.length; i++){
+   newArr[i] = sourceArray[i]*-1;
+ }
+
+ return newArr
+}
+console.log(mapToNegativize([1, 2, 3, -9]));
+function mapToNoChange(sourceArray){
+  return sourceArray
+}
+function mapToDouble(sourceArray){
+  let newArr= [];
+  for(let i = 0; i < sourceArray.length; i++){
+    newArr[i] = sourceArray[i]*2;
+  }
+  return newArr
+}
+function mapToSquare(sourceArray){
+  let newArr= [];
+  for(let i = 0; i < sourceArray.length; i++){
+    newArr[i] = sourceArray[i]**2;
+  }
+  return newArr
+}
+function reduceToTotal(sourceArray, startingPoint = 0){
+  for(let i = 0; i < sourceArray.length; i++){
+    startingPoint += sourceArray[i];
+  }
+  return startingPoint
+}
+function reduceToAllTrue(sourceArray){
+  let startingPoint = true;
+  for(let i = 0; i < sourceArray.length; i++){
+    startingPoint &= !!sourceArray[i];
+  }
+  return !!startingPoint
+}
+function reduceToAnyTrue(sourceArray){
+  let startingPoint = false;
+  for(let i = 0; i < sourceArray.length; i++){
+    startingPoint |= !!sourceArray[i];
+  }
+  return !!startingPoint
+}
+console.log(reduceToAllTrue([false,true,true])); 
